3e6873e30076ba914927bf45264cb04231f97e7b
Мясное котясо <nadya.kott@gmail.com>
2018-09-18 15:21:49 +0500

    .gitignore fix

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_RoIacnB3EZe1jCKdxMMYaGmF_kgKAzY",
+    authDomain: "elegant-skein-175414.firebaseapp.com",
+    databaseURL: "https://elegant-skein-175414.firebaseio.com",
+    projectId: "elegant-skein-175414",
+    storageBucket: "elegant-skein-175414.appspot.com",
+    messagingSenderId: "117855012805"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
